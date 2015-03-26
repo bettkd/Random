97a64e6678bb1226845cfe4f0e33af7ad2e6a20f
Dominic Bett <dominicbett@hotmail.com>
2015-03-25 17:44:33 -0700

    String compressor. finished.

--- stringCompressor.py
+++ stringCompressor.py
@@ -2,50 +2,61 @@
 # Author: Dominic Bett
 # Compress strings
 # Example: aaaaabbcccccaa = a5b2c5a2
-
-def strCompressor(string="aaabcccccaa"):
+import sys
+def strCompressor(string=""):
 	char = [x for x in string]
 	countStr = []
-	count = []
+	strComp = ""
 	for i in range(len(char)):
 		if not countStr:
 			countStr.extend(char[i])
 			c = 1
+			strComp += char[i]
 		else:
 			if countStr[-1] == char[i]:
 				c += 1
 			else:
-				count.append(c)
+				strComp += "%d"%c
 				c = 1
+				strComp += char[i]
 				countStr.extend(char[i])
 			if i == (len(char)-1):
-				count.append(c)
+				strComp += "%d"%c
 	comprStr = ""
-	for j in range(len(count)):
-		comprStr += countStr[j]+"%d"%count[j]
-	return comprStr
+	return strComp
 
-def strUncompressor(string="a3b1c5a2"):
+def strUncompressor(string=""):
 	char = [x for x in string]
 	letter = []
 	num = ""
 	let = ""
-	for char in string:
+	for i in range(len(char)):
 		if not letter:
-			lett = char
+			lett = char[i]
+			letter.append("")
 		else:
-			if char.isdigit():
-				num += char
+			if char[i].isdigit():
+				num += char[i]
 			else:
 				letter.extend(lett for x in range(int(num)))
-				lett = char
+				lett = char[i]
 				num = ""
-	print letter
+			if i == (len(char)-1):
+				letter.extend(lett for x in range(int(num)))
+	return "".join(letter)
+def main(argv):
+	if len(argv)>1:
+		str0 = argv[1]
+	else:
+		str0 = "aaaabbbbcccccccdd"
+	str1 = strCompressor(string=str0)
+	str2 = strUncompressor(string=str1)
+	print "Initial:      %s"%str0
+	print "Compressed:   %s"%str1
+	print "Uncompressed: %s"%str2
+
+if __name__ == '__main__':
+	main(sys.argv)
 
 
-print strCompressor()
-strUncompressor()
-
-#print countStr
-#print count
 
